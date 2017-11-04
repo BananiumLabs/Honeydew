e0ea8aa8eafbbf7f9a436f4638bc0e7452676dc7
Omar Hossain <omarepic1@gmail.com>
2017-11-04 16:21:17 -0700

    Add Shared folder and empty homework prediction typescript script

---
+++ src/app/shared/hw-prediction.ts
@@ -0,0 +1,10 @@
+// Omar Hoosain
+// hw-prediction.ts
+/**
+ * Use Machine Learning Linear Regression
+ * and data from previous findings of time spent on hw
+ * to make an accurate algorithm that can predict how much time
+ * a certain subject will take
+ */ 
+
+ 
