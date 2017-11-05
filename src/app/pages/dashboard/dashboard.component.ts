b820b7de90b732ed40d6f75808a365d39f395381
Eric Qian <eric@enumc.com>
2017-11-04 23:13:17 -0700

    nonworking graph

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,20 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import { AuthService } from '../../shared/auth.service';
 import { UserInfo } from '../../shared/user-info';
 import { Observable, BehaviorSubject } from 'rxjs';
 import {Router} from '@angular/router';
+import { NbThemeService } from '@nebular/theme';
 
 @Component({
   selector: 'ngx-dashboard',
   styleUrls: ['./dashboard.component.scss'],
   templateUrl: './dashboard.component.html',
 })
-export class DashboardComponent {
+export class DashboardComponent implements AfterViewInit, OnDestroy {
   userInfo: Observable<UserInfo>;
   isLoggedIn = new BehaviorSubject(false);
   name: string;
 
-  constructor(private authService: AuthService, private router: Router) {
+  options: any = {};
+  themeSubscription: any;
+
+  constructor(private authService: AuthService, private router: Router, private theme: NbThemeService) {
     console.log("Displaying Dashboard");
     this.userInfo = authService.userInfo;
     this.userInfo
@@ -24,6 +28,66 @@ export class DashboardComponent {
     // this.name = this.authService.currentUserAsVar().displayName;
   }
 
+  ngAfterViewInit() {
+    this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
+
+      const colors: any = config.variables;
+      const echarts: any = config.variables.echarts;
+
+      this.options = {
+        backgroundColor: echarts.bg,
+        color: [colors.danger, colors.warning],
+        tooltip: {},
+        legend: {
+          data: ['Allocated Budget', 'Actual Spending'],
+          textStyle: {
+            color: echarts.textColor,
+          },
+        },
+        radar: {
+          name: {
+            textStyle: {
+              color: echarts.textColor,
+            },
+          },
+          indicator: [
+            { name: 'Sales', max: 6500 },
+            { name: 'Administration', max: 16000 },
+            { name: 'Information Techology', max: 30000 },
+            { name: 'Customer Support', max: 38000 },
+            { name: 'Development', max: 52000 },
+            { name: 'Marketing', max: 25000 },
+          ],
+          splitArea: {
+            areaStyle: {
+              color: 'transparent',
+            },
+          },
+        },
+        series: [
+          {
+            name: 'Budget vs Spending',
+            type: 'radar',
+            data: [
+              {
+                value: [4300, 10000, 28000, 35000, 50000, 19000],
+                name: 'Allocated Budget',
+              },
+              {
+                value: [5000, 14000, 28000, 31000, 42000, 21000],
+                name: 'Actual Spending',
+              },
+            ],
+          },
+        ],
+      };
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.themeSubscription.unsubscribe();
+  }
+  
   checkLogin(): boolean {
     //this.authService.addPT();
     if (this.authService.isLoggedInBool() === undefined || this.authService.isLoggedInBool() === null) {
